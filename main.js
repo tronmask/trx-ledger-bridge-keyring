41354ab5fdd9d6951ba736c6eae9e420d893de04
Kevin Holder <kevholderx@gmail.com>
2020-09-09 22:15:10 -0400

    tronify

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-'use strict'
-import 'babel-polyfill';
-import LedgerBridge from './ledger-bridge'
-
+"use strict";
+import "babel-polyfill";
+import LedgerBridge from "./ledger-bridge";
 
 (async () => {
-    const bridge = new LedgerBridge()
-})()
-console.log('MetaMask < = > Ledger Bridge initialized!')
+  const bridge = new LedgerBridge();
+})();
+console.log("TronMask < = > Ledger Bridge initialized!");
